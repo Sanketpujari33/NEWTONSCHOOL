49b2735109cc0f278fdc1d469080ebe220300bf7
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2023-06-03 20:41:39 +0530

    ini
    initial commit

--- Backend/00_NSCLASS/September_9/index.js
+++ Backend/00_NSCLASS/September_9/index.js
@@ -1,3 +1,44 @@
+const http=require('http');
+
+const hostname=`127.0.0.1`;
+const port=3000;
+
+const server=http.createServer((req, res)=>{
+  if (req.url == '/data') { //check the URL of the current request
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ message: "Hello World"}));  
+    res.end();  
+}else if (req.url == '/') { //check the URL of the current request
+        
+    // set response header
+    res.writeHead(200, { 'Content-Type': 'text/html' }); 1
+    
+    // set response content    
+    res.write('<html><body><p>This is home Page.</p></body></html>');
+    res.end();
+
+}
+else if (req.url == "/student") {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><p>This is student Page.</p></body></html>');
+    res.end();
+
+}
+else if (req.url == "/admin") {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><p>This is admin Page.</p></body></html>');
+    res.end();
+
+}
+else
+    res.end('Invalid Request!');
+});
+  server.listen(port, hostname, ()=>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
 // const {helper, helper}=require('./helper');
 
 const help = require("./helper");
@@ -7,3 +48,6 @@ function firstFunction() {
   console.log("This is my first program");
 }
 firstFunction();
+var myLogModule = require('./log.js');
+
+myLogModule.info('Node.js started');
---
+++ DSA/Java_Script/01_Array/array_traversing.js
@@ -0,0 +1,7 @@
+let data=[9,45,2,8,45,23,7,78,0,11,41,77];
+
+// for(let i=0; i<data.length; i++){
+//     document.write(data[i]+" ");
+
+// }
+document.write(data[9]);
